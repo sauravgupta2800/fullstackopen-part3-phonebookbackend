025d3d2d41b7e913e6b27e30e09144b40a967a20
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-18 00:11:27 +0530

    Procfile

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+const cors = require("cors");
+app.use(cors());
+
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(
@@ -72,7 +75,7 @@ app.post("/api/persons", (request, response) => {
   response.status(201).send(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
