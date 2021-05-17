1c9c9a9b84893612ea41a7803ca3446b095e390f
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-17 20:29:33 +0530

    3.7: Phonebook backend step7: morgan middleware added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { request, response } = require("express");
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 const getRandomId = () => {
