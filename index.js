6a80574a2c1a962a93e4f2d498d15b268c391bc9
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-17 15:41:46 +0530

    3.1: Phonebook backend step1

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+let persons = [
+  { id: 1, name: "ab", number: 134 },
+  { id: 2, name: "ddd", number: 1322224 },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World123!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.send(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
