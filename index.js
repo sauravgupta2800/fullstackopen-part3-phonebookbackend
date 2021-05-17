2778f00faf1857e7782fe9f43bfb1e1b1aaf9e5b
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-17 23:32:54 +0530

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-
-app.use(morgan("tiny"));
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(
+    ":method :url :status :response-time ms - :res[content-length] :body - :req[content-length]"
+  )
+);
 app.use(express.json());
 
 const getRandomId = () => {
