7c75ead49cb0a02aa60a5374a0a0a61fe9c4a3d0
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-17 20:15:49 +0530

    3.4, 3.5 and 3.6

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+const getRandomId = () => {
+  return Math.floor(Math.random() * 99999);
+};
+
 let persons = [
   { id: 1, name: "ab", number: 134 },
   { id: 2, name: "ddd", number: 1322224 },
@@ -32,6 +36,33 @@ app.get("/api/info", (request, response) => {
     <div>Phonebook has info for ${persons.length} people</div>
     <div>${new Date()}</div>
     </div>`);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  let person = request.body;
+  const { name = "", number = 0 } = person;
+  if (!name || !number) {
+    response.status(404).send({
+      error: "Name or / and number is missing",
+    });
+  }
+  if (persons.find((person) => person.name === name)) {
+    response.status(404).send({
+      error: "Name must be unique",
+    });
+  }
+  person = {
+    ...person,
+    id: getRandomId(),
+  };
+  persons.push(person);
+  response.status(201).send(person);
 });
 
 const PORT = 3001;
