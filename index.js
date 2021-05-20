8cd518fc48338d9dbf7d64ec378ca5f3fb6332c6
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-20 10:29:56 +0530

    Exercises 3.15.-3.18.

--- index.js
+++ index.js
@@ -27,30 +27,31 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  //   const id = Number(request.params.id);
-  //   const person = persons.find((person) => person.id == id);
-  //   if (person) {
-  //     response.send(person);
-  //   } else {
-  //     response.status(404).end();
-  //   }
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) response.json(person);
+      else response.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/info", (request, response) => {
-  response.send(`<div>
-    <div>Phonebook has info for ${persons.length} people</div>
-    <div>${new Date()}</div>
-    </div>`);
+  Person.find({}).then((persons) => {
+    response.send(`<div>
+        <div>Phonebook has info for ${persons.length} people</div>
+        <div>${new Date()}</div>
+        </div>`);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -60,16 +61,38 @@ app.post("/api/persons", (request, response) => {
       error: "Name or / and number is missing",
     });
   }
-  //   if (persons.find((person) => person.name === name)) {
-  //     response.status(404).send({
-  //       error: "Name must be unique",
-  //     });
-  //   }
   const person = new Person({ name, number });
   person.save().then((savedPerson) => {
     response.status(201).send(savedPerson);
   });
 });
+
+app.put("/api/persons/:id", (request, response) => {
+  const { name = "", number = 0 } = request.body;
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true }
+  ).then((savedPerson) => {
+    response.status(201).send(savedPerson);
+  });
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+// handler of requests with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
